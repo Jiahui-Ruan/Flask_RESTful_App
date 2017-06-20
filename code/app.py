70a82fac51613cadb33cd3f286f874f5612ad9aa
Jiahui Ruan <jr58@rice.edu>
2017-06-19 22:20:45 -0700

    finish this section

--- code/app.py
+++ code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
@@ -7,25 +7,49 @@ app = Flask(__name__)
 app.secret_key = 'Ryan'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity) # /auth create a new endpoint
+jwt = JWT(app, authenticate, identity) # JWT create a new endpoint /auth
 
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank"
+    )
+
     @jwt_required()
     def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = next(iter(filter(lambda x: x['name'] == name, items)), None)
         # filter function return a filter object and use list will return list, next will return the first one
         return {'item': item}, 200 if item else 404
 
-    @jwt_required
     def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
+        if next(iter(filter(lambda x: x['name'] == name, items)), None):
             return {'message': "An item with name '{}' is already exists.".format(name)}, 400 # bad request already have one inside
-        data = request.get_json() # use force = True will automatically look into the request content, so user don't have to provide the content-type header
+
+        data = Item.parser.parse_args() # use force = True will automatically look into the request content, so user don't have to provide the content-type header
+
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = next(iter(filter(lambda x: x['name'] == name, items)), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 class ItemList(Resource):
     def get(self):
